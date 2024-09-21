0009d2fa8152eb6442be0d181567e89cf515319d
Youbuu2 <bijan121@verizon.net>
2024-09-20 21:57:28 -0700

    add all new files

---
+++ HistogramOfEmptySpace.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define IN 1   /* inside a word */
+#define OUT 0  /* outside a word */
+
+/* count lines, words, and characters in input */
+int main(void)
+{
+    int c, state;
+
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t'){
+            state = OUT;
+            printf("*");
+        }
+        else if(state == OUT) {
+            state = IN;
+            //printf("*");
+        }
+        //else
+        //printf("*");
+    }
+    return 0;
+}
